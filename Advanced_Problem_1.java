e800fcd08f32e258c82bb926fae24d9e2581222b
Arpit Dhiman <arpitdhimann@gmail.com>
2026-07-10 21:43:32 +0530

    feat: implement string permutation generator using recursion

--- Advanced_Problem_1.java
+++ Advanced_Problem_1.java
@@ -1,21 +1,31 @@
-//PROBLEM: Print all the permutations of a string.
+// PROBLEM: Print all the permutations of a string.
 
 public class Advanced_Problem_1 {
-   public static void printPermutation(String str, int idx, String perm) {
-       if(str.length() == 0) {
-           System.out.println(perm);
-           return;
-       }
-     
-       for(int i=0; i<str.length(); i++) {
-           char currChar = str.charAt(i);
-           String newStr = str.substring(0, i) + str.substring(i+1);
-           printPermutation(newStr, idx+1, perm+currChar);
-       }
-   }
 
-       public static void main(String args[]) {
-           String str = "abc";
-           printPermutation(str, 0, "");
-   }
+    // Function to generate all permutations
+    // str: current available string, perm: string built so far
+    public static void printPermutation(String str, int idx, String perm) {
+        // Base case: If no characters left in the input string, print the permutation
+        if(str.length() == 0) {
+            System.out.println(perm);
+            return;
+        }
+       
+        // Recursive loop: Pick each character once and create a new string excluding it
+        for(int i = 0; i < str.length(); i++) {
+            char currChar = str.charAt(i);
+            
+            // newStr contains everything except the picked character
+            String newStr = str.substring(0, i) + str.substring(i + 1);
+            
+            // Recursive call with reduced string and updated permutation
+            printPermutation(newStr, idx + 1, perm + currChar);
+        }
+    }
+
+    public static void main(String args[]) {
+        String str = "abc";
+        // Start recursion with empty permutation string
+        printPermutation(str, 0, "");
+    }
 }
